d4f41efea05e543366a32394ec6d24e66d94cb81
Vicky Fransisco <vickyfrans1@gmail.com>
2024-03-07 15:37:31 +0700

    fix some bug and add UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,59 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     public static GameManager Instance { get; private set; }
+
     [SerializeField] private Player player;
+    [SerializeField] private Text scoreText;
     [SerializeField] private GameObject playButton;
     [SerializeField] private GameObject gameOver;
+    private float startTime;
 
     private int score;
     public int Score => score;
 
-    private void Awake()
-    {
-        if (Instance != null)
-        {
+    private void Awake() {
+        if (Instance != null) {
             DestroyImmediate(gameObject);
-        }
-        else
-        {
+        } else {
             Instance = this;
             Application.targetFrameRate = 60;
             DontDestroyOnLoad(gameObject);
-            // Pause();
+            Pause();
         }
     }
+
     public void Play() {
-        // score = 0;
-        // scoreText.text = score.ToString();
+        score = 0;
+        scoreText.text = score.ToString();
 
-        // playButton.SetActive(false);
-        // gameOver.SetActive(false);
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
 
+        startTime = Time.time;
+        
         Time.timeScale = 1f;
         player.enabled = true;
 
-        Lobang[] lobang = FindObjectsOfType<Lobang>();
+         Lobang[] lobangs = FindObjectsOfType<Lobang>();
 
-        for (int i = 0; i < lobang.Length; i++) {
-            Destroy(lobang[i].gameObject);
+        for (int i = 0; i < lobangs.Length; i++) {
+            Destroy(lobangs[i].gameObject);
         }
     }
 
     public void GameOver() {
-        // playButton.SetActive(true);
-        // gameOver.SetActive(true);
-        Debug.Log("Game Over Trigered");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
         Pause();
     }
 
-    public void Pause() {
-        Debug.Log("Game Over Trigered");
+    public void IncreaseScore()
+    {
+        float elapsedTime = Time.time - startTime;
+        int scoreToAdd = Mathf.FloorToInt(elapsedTime * 5f);
+        if (scoreToAdd > score) {
+            score = scoreToAdd;
+            scoreText.text = score.ToString();
+        }
+    }
 
+     public void Pause()
+    {
         Time.timeScale = 0f;
         player.enabled = false;
     }
+
 }
